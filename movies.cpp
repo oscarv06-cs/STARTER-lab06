676504a112df9751486511d5a4a26eeeb9a8f3b0
Oscar Ramon Valeriano Leiva <oscarvaleriano@csilvm-02.cs.ucsb.edu>
2025-05-23 16:25:46 -0700

    started movies cpp

--- movies.cpp
+++ movies.cpp
@@ -1,1 +1,12 @@
 #include "movies.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorith>
+
+void Movies::readCSV(const std::string& filename){
+    istd::ifstream file_input(filename);
+    if 
+
+}
+
